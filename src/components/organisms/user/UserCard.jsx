93ffe40844f7e3a17cd13bc0045ce2cfe865861e
Harune Odagiri <harune.odagiri@nijibox.co.jp>
2023-01-05 00:31:21 +0000

    セクション6 Atomic Design（Organism）UserCardコンポーネントの効率化

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,16 +1,20 @@
 import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
 import styled from "styled-components";
 export const UserCard = (props) => {
   const { user } = props;
   return (
     <Card>
-      <img
-        src={user.image}
-        alt={`${user.image}のプロフィール写真`}
-        width={160}
-        height={160}
-      />
-      <p>{user.name}</p>
+      <UserIconWithName image={user.image} name={user.name} />
+      {/* <UserIconWithName>
+        <img
+          src={user.image}
+          alt={`${user.name}のプロフィール写真`}
+          width={160}
+          height={160}
+        />
+        <p>{user.name}</p>
+      </UserIconWithName> */}
       <SDl>
         <dt>メール</dt>
         <dd>{user.email}</dd>
